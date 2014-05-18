1cc1990dde4fa66687cd840e1d7d8e100edb4976
Tiago Antao <tiagoantao@gmail.com>
2014-05-18 19:06:13 +0100

    support for multi-page output

--- src/abjad-nb.py
+++ src/abjad-nb.py
@@ -10,23 +10,42 @@ from abjad.tools import systemtools, topleveltools
 
 
 def _get_png(expr):
+    pngs = []
     tmpdir = tempfile.mkdtemp()
     agent = topleveltools.persist(expr)
     result = agent.as_ly(tmpdir + os.sep + 'out.ly')
     ly_file_path, abjad_formatting_time = result
     cmd = 'lilypond --png -o%s/out %s/out.ly' % (tmpdir, tmpdir)
     result = systemtools.IOManager.spawn_subprocess(cmd)
-    img = wimg(filename=tmpdir + os.sep + 'out.png')
-    img.trim()
-    img.save(filename=tmpdir + os.sep + 'trim.png')
-    f = open(tmpdir + os.sep + 'trim.png', 'rb')
-    png = f.read()
-    f.close()
+    try:
+        imgs = [wimg(filename=tmpdir + os.sep + 'out.png')]
+    except:
+        #This might be a multipage output
+        #We are going to send all pages
+        imgs = []
+        for i in range(1000):  # Lets hope you do not have more than 1000 pages
+            try:
+                imgs.append(wimg(filename=tmpdir + os.sep +
+                                 'out-page%d.png' % (i + 1)))
+            except:
+                if i == 0:  # No images
+                    raise
+                else:
+                    break
+  
+    for img in imgs:
+        img.trim()
+        img.save(filename=tmpdir + os.sep + 'trim.png')
+        f = open(tmpdir + os.sep + 'trim.png', 'rb')
+        png = f.read()
+        pngs.append(png)
+        f.close()
     shutil.rmtree(tmpdir)
-    return png
+    return pngs
 
 
 def show(expr):
     assert '__illustrate__' in dir(expr)
-    png = _get_png(expr)
-    display_png(png, raw=True)
+    pngs = _get_png(expr)
+    for png in pngs:
+        display_png(png, raw=True)
